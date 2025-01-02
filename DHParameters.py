2cc7ee0809ffdb571843b01c63c377031e410f51
Lehu Lee <leihui.li@outlook.com>
2025-01-02 21:27:05 +0800

    add all required robot arm

--- DHParameters.py
+++ DHParameters.py
@@ -1,10 +1,11 @@
 import numpy as np
 from numpy.linalg import inv
+from scipy.spatial.transform import Rotation as R
 
 
 class DHParameters:
-    def __init__(self):
-        pass
+    def __init__(self, robot_name: str):
+        self.robot_name = robot_name
         # self.create_DH_parameters()
 
     def create_DH_parameters(self, joints: list):
@@ -17,20 +18,35 @@ class DHParameters:
             joints = joints + [0] * (7 - len(joints))
         print(f"joints: {joints}")
         # joints = joints
+        # modified DH parameters [a, alpha, d, theta]
         DH = {
-            "Franka Research 3": {
+            "Franka Panda": {
                 "type": "modified",
                 "resource": "https://frankaemika.github.io/docs/control_parameters.html#denavithartenberg-paramete",
                 "DH": np.matrix(
-                    # [a, d, alpha, theta]
                     [
-                        [0, 0.333, 0, joints[0]],
-                        [0, 0, -np.pi / 2, joints[1]],
-                        [0, 0.316, np.pi / 2, joints[2]],
-                        [0.0825, 0, np.pi / 2, joints[3]],
-                        [-0.0825, 0.384, -np.pi / 2, joints[4]],
-                        [0, 0, np.pi / 2, joints[5]],
-                        [0.088, 0, np.pi / 2, joints[6]],
+                        [0, 0, 333 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, joints[1]],
+                        [0, np.pi / 2, 316 / 1000.0, joints[2]],
+                        [82.5 / 1000.0, np.pi / 2, 0, joints[3]],
+                        [-82.5 / 1000.0, -np.pi / 2, 384 / 1000.0, joints[4]],
+                        [0, np.pi / 2, 0, joints[5]],
+                        [88 / 1000.0, np.pi / 2, 107 / 1000.0, joints[6]],
+                    ]
+                ),
+            },
+            "Franka Franka 3": {  # this is identical to Franka Panda
+                "type": "modified",
+                "resource": "https://frankaemika.github.io/docs/control_parameters.html#denavithartenberg-parameters",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 333 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, joints[1]],
+                        [0, np.pi / 2, 316 / 1000.0, joints[2]],
+                        [82.5 / 1000.0, np.pi / 2, 0, joints[3]],
+                        [-82.5 / 1000.0, -np.pi / 2, 384 / 1000.0, joints[4]],
+                        [0, np.pi / 2, 0, joints[5]],
+                        [88 / 1000.0, np.pi / 2, 107 / 1000.0, joints[6]],
                     ]
                 ),
             },
@@ -62,25 +78,169 @@ class DHParameters:
                     ]
                 ),
             },
+            "Universe Robot 10e": {
+                "type": "modified",
+                "resource": "https://www.universal-robots.com/articles/ur/application-installation/dh-parameters-for-calculations-of-kinematics-and-dynamics/",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 0.1807, joints[0]],
+                        [0, np.pi / 2, 0, joints[1]],
+                        [-0.6127, 0, 0, joints[2]],
+                        [-0.57155, 0, 0.17415, joints[3]],
+                        [0, np.pi / 2, 0.11985, joints[4]],
+                        [0, -np.pi / 2, 0.11655, joints[5]],
+                    ]
+                ),
+            },
+            "KUKA iiwa 7": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 340 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, joints[1]],
+                        [0, np.pi / 2, 400 / 1000.0, joints[2]],
+                        [0, -np.pi / 2, 0, -joints[3]],
+                        [0, np.pi / 2, 400 / 1000.0, joints[4]],
+                        [0, -np.pi / 2, 0, joints[5]],
+                        [0, np.pi / 2, 130 / 1000.0, joints[6]],
+                    ]
+                ),
+            },
+            "KUKA iiwa 14": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 360 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, joints[1]],
+                        [0, np.pi / 2, 420 / 1000.0, joints[2]],
+                        [0, -np.pi / 2, 0, joints[3]],
+                        [0, np.pi / 2, 400 / 1000.0, joints[4]],
+                        [0, -np.pi / 2, 0, joints[5]],
+                        [0, np.pi / 2, 126 / 1000.0, joints[6]],
+                    ]
+                ),
+            },
             "Kinova Gen3": {
                 "type": "modified",
                 "resource": "https://github.com/SaltworkerMLU/RobotKinematicsCatalogue",
                 "DH": np.matrix(
                     [
-                        [0, 0, 284.8 / 1000.0, -joints[0]],
-                        [0, -np.pi / 2, -11.75 / 1000.0, joints[1]],
-                        [0, np.pi / 2, 420.75 / 1000.0, -joints[2]],
-                        [0, -np.pi / 2, -12.75 / 1000.0, joints[3]],
-                        [-0.35 / 1000.0, np.pi / 2, 314.35 / 1000.0, -joints[4]],
+                        [0, 0, 284.8 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 1, -np.pi / 2 + joints[1]],
+                        [410 / 1000.0, 0, 0, -np.pi / 2 - joints[2]],
+                        [0, -np.pi / 2, 314.3 / 1000.0, -joints[3]],
+                        [0, np.pi / 2, 0, -joints[4]],
+                        [0, -np.pi / 2, 167.4 / 1000.0, -joints[5]],
+                    ]
+                ),
+            },
+            "UFACTORY xArm5": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 267 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, np.deg2rad(-79.35) + joints[1]],
+                        [289.489 / 1000.0, 0, 0, np.deg2rad(79.35) + joints[2]],
+                        [77.5 / 1000.0, -np.pi / 2, 342.5 / 1000.0, joints[3]],
+                        [0, np.pi / 2, 0, joints[4]],  # always theta5 = np.pi/2
+                        [76 / 1000.0, -np.pi / 2, 97 / 1000.0, joints[5]],
+                    ]
+                ),
+            },
+            "UFACTORY xArm6": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 267 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, -np.deg2rad(79.35) + joints[1]],
+                        [289.489 / 1000.0, 0, 0, np.deg2rad(79.35) + joints[2]],
+                        [77.5 / 1000.0, -np.pi / 2, 342.5 / 1000.0, joints[3]],
+                        [0, np.pi / 2, 0, joints[4]],
+                        [76 / 1000.0, -np.pi / 2, 97 / 1000.0, joints[5]],
+                    ]
+                ),
+            },
+            "UFACTORY xArm7": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 267 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, joints[1]],
+                        [0, np.pi / 2, 293 / 1000.0, joints[2]],
+                        [52.2 / 1000.0, -np.pi / 2, 0, np.pi - joints[3]],
+                        [-77.5 / 1000.0, np.pi / 2, 342.5 / 1000.0, joints[4]],
                         [0, -np.pi / 2, 0, joints[5]],
-                        [0, np.pi / 2, 170.35 / 1000.0, -joints[6]],
+                        [-76 / 1000.0, np.pi / 2, 97 / 1000.0, np.pi + joints[6]],
+                    ]
+                ),
+            },
+            "ABB YuMI IRB": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 305 / 1000.0, joints[0]],
+                        [-30 / 1000.0, -np.pi / 2, 0, joints[1]],
+                        [30 / 1000.0, np.pi / 2, 251.5 / 1000.0, joints[2]],
+                        [40.5 / 1000.0, -np.pi / 2, 0, np.pi / 2 + joints[3]],
+                        [-40.5 / 1000.0, np.pi / 2, 265 / 1000.0, joints[4]],
+                        [27 / 1000.0, -np.pi / 2, 0, joints[5]],
+                        [-27 / 1000.0, np.pi / 2, 36 / 1000.0, joints[6]],
+                    ]
+                ),
+            },
+            "ABB CRB 15000": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 265 / 1000.0, joints[0]],
+                        [0, -np.pi / 2, 0, -np.pi / 2 + joints[1]],
+                        [444 / 1000.0, 0, 0, joints[2]],
+                        [110 / 1000.0, -np.pi / 2, 470 / 1000.0, joints[3]],
+                        [0, np.pi / 2, 0, joints[4]],
+                        [80 / 1000.0, -np.pi / 2, 101 / 1000.0, np.pi + joints[5]],
+                    ]
+                ),
+            },
+            "FUNC CRX-5iA": {
+                "type": "modified",
+                "resource": "",
+                "DH": np.matrix(
+                    [
+                        [0, 0, 0, joints[0]],
+                        [0, -np.pi / 2, 0, -np.pi / 2 + joints[1]],
+                        [410 / 1000.0, 0, 0, -joints[2]],
+                        [0, -np.pi / 2, 430 / 1000.0, -joints[3]],
+                        [0, np.pi / 2, -130 / 1000.0, -joints[4]],
+                        [0, -np.pi / 2, 145 / 1000.0, -joints[5]],
+                    ]
+                ),
+            },
+            "Sawyer": {
+                "type": "modified",
+                "resource": "https://petercorke.github.io/robotics-toolbox-python/arm_dh.html#roboticstoolbox.models.DH.Sawyer",
+                "DH": np.matrix(
+                    [
+                        [0.081, -np.pi / 2, 0.317, joints[0]],
+                        [0, -np.pi / 2, 0.1925, joints[1]],
+                        [0, -np.pi / 2, 0.4, joints[3]],
+                        [0, -np.pi / 2, 0.1685, joints[4]],
+                        [0, -np.pi / 2, 0.4, joints[5]],
+                        [0, -np.pi / 2, 0.1363, joints[6]],
+                        [0, 0, 0.1338, joints[6]],
                     ]
                 ),
             },
         }
         return DH
 
-    def get_transformation_matrix(self, type, data):
+    def calculate_matrix(self, type, data):
         """
         Returns the transformation matrix for a single link
         """
@@ -117,30 +277,28 @@ class DHParameters:
         else:
             raise ValueError("Invalid type")
 
-    def get_transformations(self, robot_name: str, joint_angles: list):
+    def get_transformations(self, joint_angles: list):
         DH = self.create_DH_parameters(joint_angles)
-        assert robot_name in DH.keys(), "Robot not found in DH parameters"
+        assert self.robot_name in DH.keys(), "Robot not found in DH parameters"
 
-        DH_params = DH[robot_name]["DH"]
-        DH_type = DH[robot_name]["type"]
+        DH_params = DH[self.robot_name]["DH"]
+        DH_type = DH[self.robot_name]["type"]
+        # print(DH_params)
+        # exit()
 
         T_total = np.identity(4)
+
         for i in range(DH_params.shape[0]):
-
             data = DH_params[i, 0], DH_params[i, 1], DH_params[i, 2], DH_params[i, 3]
-
-            T_i = self.get_transformation_matrix(DH_type, data)
-
+            T_i = self.calculate_matrix(DH_type, data)
             T_total = np.dot(T_total, T_i)
-
+            # print(f"{i}# {T_total}")
+        # print(T_total)
         return T_total
 
     def matrix2RXYZ(self, matrix):
-        matrix = matrix[:3, :3]
-        beta = np.arctan2(-matrix[2, 0], np.sqrt(matrix[0, 0] * matrix[0, 0] + matrix[1, 0] * matrix[1, 0]))
-        alpha = np.arctan2(matrix[1, 0] / np.cos(beta), matrix[0, 0] / np.cos(beta))
-        r = np.arctan2(matrix[2, 1] / np.cos(beta), matrix[2, 2] / np.cos(beta))
-        return r, beta, alpha
+        r = R.from_matrix(matrix[:3, :3])
+        return r.as_euler("xyz", degrees=False)
 
     def matrix2TXYZ(self, matrix):
         Tx, Ty, Tz = matrix[:3, 3] * 1000
--- main.py
+++ main.py
@@ -4,12 +4,25 @@ import numpy as np
 np.set_printoptions(suppress=True, precision=6)
 
 if __name__ == "__main__":
-    dh = dh.DHParameters()
-    # res = dh.get_transformations("Universe Robot 3e", [0, 0, 10, 0, 0, 0])
-    res = dh.get_transformations("Universe Robot 5e", [0, 0, 10, 0, 0, 0])
-    # res = dh.get_transformations("Kinova Gen3", [10, 0, 0, 0, 0, 0, 0])
+    # robot_name = "Universe Robot 3e"
+    # robot_name = "Universe Robot 5e"
+    # robot_name = "Universe Robot 10e"
+    # robot_name = "Franka Panda"
+    # robot_name = "Kinova Gen3"
+    # robot_name = "KUKA iiwa 7"
+    # robot_name = "KUKA iiwa 14"
+    # robot_name = "UFACTORY xArm5"
+    # robot_name = "UFACTORY xArm6"
+    # robot_name = "UFACTORY xArm7"
+    # robot_name = "ABB YuMI IRB"
+    # robot_name = "ABB CRB 15000"
+    robot_name = "FUNC CRX-5iA"
+    # robot_name = "Sawyer"
+    dh = dh.DHParameters(robot_name)
+    # res = dh.get_transformations([0, 0, 0, 0, 0, 0, 0])
+    res = dh.get_transformations([0, 0, 20, 8, 100, 0])
     r, beta, alpha = dh.matrix2RXYZ(res)
-    print(f"deg: {np.rad2deg(r):.6f} {np.rad2deg(beta):.6f} {np.rad2deg(alpha):.6f}")
+    print(f"deg: {np.rad2deg(r):.1f} {np.rad2deg(beta):.1f} {np.rad2deg(alpha):.1f}")
     Tx, Ty, Tz = dh.matrix2TXYZ(res)
-    print(f"Tx, Ty, Tz (mm): {Tx:.6f} {Ty:.6f} {Tz:.6f}")
+    print(f"Tx, Ty, Tz (mm): {Tx:.3f} {Ty:.3f} {Tz:.3f}")
     # print(res)
